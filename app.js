30ab235b88869315938b93aec703eb329d400bda
Mohd-Rahmath <uniquedevv@gmail.com>
2022-06-14 12:47:33 +0530

    code

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome to this world</h1>')
+})
+
+
+app.get('/product', (req, res) => {
+    res.send([{
+            product_id: "101",
+            price: "100"
+        },
+        {
+            product_id: "102",
+            price: "200"
+        }
+    ])
+})
+
+
+app.listen(port, () => {
+    console.log(`demo app is used,port:${port}`);
+})
