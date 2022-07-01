e76f69bf1d9134035723cbf48a7ea7788653fb84
Mohd-Rahmath <uniquedevv@gmail.com>
2022-07-01 20:02:19 +0530

    code

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+
+!#/usr/bin/bash -xev
+sudo apt-get update
+sudo apt-get install wget
+cd /home/ubuntu
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+sudo chmod +x ./install
+sudo ./install auto
+
+
+
 #Stopping existing node servers
 echo "Stopping any existing node servers"
 pkill node
